a2b6c0ff540c23c9ea6c92e097984cab1dcff019
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-08 00:56:04 +0530

    header changed to white color

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,51 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
-import { Badge, Grid, IconButton, InputBase, Toolbar } from "@material-ui/core";
+import {
+  Badge,
+  Grid,
+  IconButton,
+  InputBase,
+  makeStyles,
+  Toolbar,
+} from "@material-ui/core";
 import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
 import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
 import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew";
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: "#fff",
+  },
+});
 function Header() {
+  const classes = useStyles();
   return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Grid container>
-            <Grid item>
-              <InputBase />
-            </Grid>
-            <Grid item sm></Grid>
-            <Grid item>
-              <IconButton>
-                <Badge badgeContent={6} color="secondary">
-                  <NotificationsNoneIcon />
-                </Badge>
-              </IconButton>
-              <IconButton>
-                <Badge badgeContent={6} color="secondary">
-                  <ChatBubbleOutlineIcon />
-                </Badge>
-              </IconButton>
-              <IconButton>
-                <Badge color="secondary">
-                  <PowerSettingsNewIcon />
-                </Badge>
-              </IconButton>
-            </Grid>
+    <AppBar position="static" className={classes.root}>
+      <Toolbar>
+        <Grid container>
+          <Grid item>
+            <InputBase />
           </Grid>
-        </Toolbar>
-      </AppBar>
-    </div>
+          <Grid item sm></Grid>
+          <Grid item>
+            <IconButton>
+              <Badge badgeContent={6} color="secondary">
+                <NotificationsNoneIcon />
+              </Badge>
+            </IconButton>
+            <IconButton>
+              <Badge badgeContent={6} color="secondary">
+                <ChatBubbleOutlineIcon />
+              </Badge>
+            </IconButton>
+            <IconButton>
+              <Badge color="secondary">
+                <PowerSettingsNewIcon />
+              </Badge>
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
   );
 }
 

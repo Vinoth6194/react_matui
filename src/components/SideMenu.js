f7525ca47357c2b90667023fe1470a5ccf292a9a
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-07 20:51:36 +0530

    sidebar styled using makestyles in jss

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,7 +1,21 @@
+import { makeStyles } from "@material-ui/core";
 import React from "react";
 
 function SideMenu() {
-  return <div className="side-menu"></div>;
+  const useStyles = makeStyles({
+    sideMenu: {
+      display: "flex",
+      flexDirection: "column",
+      position: "absolute",
+      left: "0px",
+      width: "320px",
+      height: "100%",
+      backgroundColor: "#253053",
+    },
+  });
+  const classes = useStyles();
+  console.log(classes);
+  return <div className={classes.sideMenu}></div>;
 }
 
 export default SideMenu;

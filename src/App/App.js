df7be408a32aaba9b0dd82a6e714eb349d75557b
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-08 01:36:02 +0530

    theme created and applied

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,27 @@
-import { CssBaseline, makeStyles } from "@material-ui/core";
+import {
+  createMuiTheme,
+  CssBaseline,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core";
 import Header from "../components/Header";
 import SideMenu from "../components/SideMenu";
 import "./App.css";
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#333996",
+      light: "#3c44b126",
+    },
+    secondary: {
+      main: "#f83245",
+      light: "#f8324526",
+    },
+    background: {
+      default: "#f4f5fd",
+    },
+  },
+});
 const useStyes = makeStyles({
   appMain: {
     paddingLeft: "320px",
@@ -11,14 +31,13 @@ const useStyes = makeStyles({
 function App() {
   const classes = useStyes();
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <SideMenu />
       <div className={classes.appMain}>
         <Header />
         <CssBaseline />
       </div>
-      ;
-    </>
+    </ThemeProvider>
   );
 }
 

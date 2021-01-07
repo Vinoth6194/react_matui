a32f34b2244f498d2bf1c872f9a97e156c2d1f79
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-08 01:05:03 +0530

    styled search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,9 +11,15 @@ import {
 import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
 import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
 import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew";
+import SearchIcon from "@material-ui/icons/Search";
 const useStyles = makeStyles({
   root: {
     backgroundColor: "#fff",
+  },
+  searchInput: {
+    opacity: "0.6",
+    padding: "0px 8px",
+    fontSize: "0.8rem",
   },
 });
 function Header() {
@@ -21,25 +27,29 @@ function Header() {
   return (
     <AppBar position="static" className={classes.root}>
       <Toolbar>
-        <Grid container>
+        <Grid container alignItems="center">
           <Grid item>
-            <InputBase />
+            <InputBase
+              placeholder="Search"
+              startAdornment={<SearchIcon fontSize="small" />}
+              className={classes.searchInput}
+            />
           </Grid>
           <Grid item sm></Grid>
           <Grid item>
             <IconButton>
               <Badge badgeContent={6} color="secondary">
-                <NotificationsNoneIcon />
+                <NotificationsNoneIcon fontSize="small" />
               </Badge>
             </IconButton>
             <IconButton>
               <Badge badgeContent={6} color="secondary">
-                <ChatBubbleOutlineIcon />
+                <ChatBubbleOutlineIcon fontSize="small" />
               </Badge>
             </IconButton>
             <IconButton>
               <Badge color="secondary">
-                <PowerSettingsNewIcon />
+                <PowerSettingsNewIcon fontSize="small" />
               </Badge>
             </IconButton>
           </Grid>

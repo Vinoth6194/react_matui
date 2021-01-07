4926cbe327c70c9cac959d859d0d5f2c83a73397
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-07 21:08:00 +0530

    appMain in jss

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,18 @@
+import { makeStyles } from "@material-ui/core";
 import SideMenu from "../components/SideMenu";
 import "./App.css";
-import "../index.css";
+const useStyes = makeStyles({
+  appMain: {
+    paddingLeft: "320px",
+    width: "100%",
+  },
+});
 function App() {
+  const classes = useStyes();
   return (
     <>
       <SideMenu />
-      <div>Here we go</div>;
+      <div className={classes.appMain}>Here we go</div>;
     </>
   );
 }

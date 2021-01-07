a4906eebcfbc3ad5b20bddb6513e772fc239d770
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-07 21:44:59 +0530

    header - badges and searchbox

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,37 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
-import { Toolbar } from "@material-ui/core";
-
+import { Badge, Grid, IconButton, InputBase, Toolbar } from "@material-ui/core";
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew";
 function Header() {
   return (
     <div>
       <AppBar position="static">
-        <Toolbar></Toolbar>
+        <Toolbar>
+          <Grid container>
+            <Grid item sm={4} style={{ border: "1px solid red" }}>
+              <InputBase />
+            </Grid>
+            <Grid item sm={8} style={{ border: "1px solid green" }}>
+              <IconButton>
+                <Badge badgeContent={6} color="secondary">
+                  <NotificationsNoneIcon />
+                </Badge>
+              </IconButton>
+              <IconButton>
+                <Badge badgeContent={6} color="secondary">
+                  <ChatBubbleOutlineIcon />
+                </Badge>
+              </IconButton>
+              <IconButton>
+                <Badge color="secondary">
+                  <PowerSettingsNewIcon />
+                </Badge>
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Toolbar>
       </AppBar>
     </div>
   );

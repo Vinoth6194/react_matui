e52d1e2b1d63e1d09230b0d4c47f359772109fc8
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-07 21:00:09 +0530

    sidebar styled using withstyles in jss

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,21 +1,20 @@
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, withStyles } from "@material-ui/core";
 import React from "react";
 
-function SideMenu() {
-  const useStyles = makeStyles({
-    sideMenu: {
-      display: "flex",
-      flexDirection: "column",
-      position: "absolute",
-      left: "0px",
-      width: "320px",
-      height: "100%",
-      backgroundColor: "#253053",
-    },
-  });
-  const classes = useStyles();
-  console.log(classes);
+const style = {
+  sideMenu: {
+    display: "flex",
+    flexDirection: "column",
+    position: "absolute",
+    left: "0px",
+    width: "320px",
+    height: "100%",
+    backgroundColor: "#253053",
+  },
+};
+const SideMenu = (props) => {
+  const { classes } = props;
   return <div className={classes.sideMenu}></div>;
-}
+};
 
-export default SideMenu;
+export default withStyles(style)(SideMenu);

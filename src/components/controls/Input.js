1ee31b70aca5d3b153789819ff75b16fdb9e7539
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-08 17:38:50 +0530

    autocomplete off for textfields

--- src/components/controls/Input.js
+++ src/components/controls/Input.js
@@ -5,6 +5,7 @@ export default function Input(props) {
   const { name, label, value, onChange } = props;
   return (
     <TextField
+      autoComplete="off"
       variant="outlined"
       label={label}
       value={value}

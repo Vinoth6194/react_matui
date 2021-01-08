4628e061e79db46c2ced8caa4c72117e7cd48153
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-08 16:24:10 +0530

    fullname email textfields

--- src/components/pages/Employees/EmployeeForm.js
+++ src/components/pages/Employees/EmployeeForm.js
@@ -1,7 +1,58 @@
-import React from "react";
+import { Grid, makeStyles, TextField } from "@material-ui/core";
+import React, { useState } from "react";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiFormControl-root": {
+      width: "80%",
+      margin: theme.spacing(1),
+    },
+  },
+}));
+const initialFValues = {
+  id: 0,
+  fullName: "",
+  email: "",
+  mobile: "",
+  city: "",
+  gender: "male",
+  departmentId: "",
+  hireDate: new Date(),
+  isPermanent: false,
+};
 
 function EmployeeForm() {
-  return <div></div>;
+  const classes = useStyles();
+  const [values, setValues] = useState(initialFValues);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+  return (
+    <form className={classes.root}>
+      <Grid container>
+        <Grid item xs={6}>
+          <TextField
+            variant="outlined"
+            label="Full Name"
+            value={values.fullName}
+            name="fullName"
+            onChange={handleInputChange}
+          />
+          <TextField
+            variant="outlined"
+            label="Email"
+            value={values.email}
+            name="email"
+            onChange={handleInputChange}
+          />
+        </Grid>
+        <Grid item xs={6}></Grid>
+      </Grid>
+    </form>
+  );
 }
 
 export default EmployeeForm;
--- src/components/pages/Employees/Employees.js
+++ src/components/pages/Employees/Employees.js
@@ -2,8 +2,16 @@ import React from "react";
 import PageHeader from "../../PageHeader";
 import EmployeeForm from "./EmployeeForm";
 import PeopleOutlineTwoToneIcon from "@material-ui/icons/PeopleOutlineTwoTone";
-
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core";
+const useStyles = makeStyles((theme) => ({
+  pageContent: {
+    margin: theme.spacing(5),
+    padding: theme.spacing(3),
+  },
+}));
 function Employee() {
+  const classes = useStyles();
   return (
     <>
       <PageHeader
@@ -11,7 +19,9 @@ function Employee() {
         subTitle="Form design with validation"
         icon={<PeopleOutlineTwoToneIcon fontSize="large" />}
       ></PageHeader>
-      <EmployeeForm />
+      <Paper className={classes.pageContent}>
+        <EmployeeForm />
+      </Paper>
     </>
   );
 }

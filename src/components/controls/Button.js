28572503c877088dd73d3df359cb0ec8c80d06a6
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-10 02:15:43 +0530

    buttons styled with mui themes

--- src/components/controls/Button.js
+++ src/components/controls/Button.js
@@ -1,8 +1,18 @@
 import React from "react";
-import { Button as MuiButton } from "@material-ui/core";
+import { Button as MuiButton, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(0.5),
+  },
+  label: {
+    textTransform: "none",
+  },
+}));
 
 function Button(props) {
   const { text, size, color, variant, onClick, ...other } = props;
+  const classes = useStyles();
   return (
     <MuiButton
       variant={variant || "contained"}
@@ -10,6 +20,7 @@ function Button(props) {
       color={color || "primary"}
       onClick={onClick}
       {...other}
+      classes={{ root: classes.root, label: classes.label }}
     >
       {text}
     </MuiButton>

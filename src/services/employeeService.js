eddc41583b0cdd21d471e678b5247d0f9f495f50
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-10 23:23:37 +0530

    getAllEmp using localstorage

--- src/services/employeeService.js
+++ src/services/employeeService.js
@@ -1,6 +1,15 @@
+const KEYS = {
+  employees: "employees",
+};
 export const getDepartmentCollection = () => [
   { id: "1", title: "Development" },
   { id: "2", title: "Marketing" },
   { id: "3", title: "Accounting" },
   { id: "4", title: "HR" },
 ];
+
+export function getAllEmployess() {
+  if (localStorage.getItem(KEYS.employees) == null)
+    localStorage.setItem(KEYS.employees, JSON.stringify([]));
+  return JSON.parse(localStorage.getItem(KEYS.employees));
+}

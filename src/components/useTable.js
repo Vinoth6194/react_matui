df1829a471f52ad4e90920d7857cfab126be6686
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-11 00:50:05 +0530

    table styled

--- src/components/useTable.js
+++ src/components/useTable.js
@@ -1,8 +1,33 @@
-import { Table, TableCell, TableHead, TableRow } from "@material-ui/core";
+import {
+  makeStyles,
+  Table,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@material-ui/core";
 import React from "react";
-
+const useStyles = makeStyles((theme) => ({
+  table: {
+    marginTop: theme.spacing(3),
+    "& thead th": {
+      fontWeight: "600",
+      color: theme.palette.primary.main,
+      backgroundColor: theme.palette.primary.light,
+    },
+    "& tbody td": {
+      fontWeight: "300",
+    },
+    "& tbody tr:hover": {
+      backgroundColor: "#fffbf2",
+      cursor: "pointer",
+    },
+  },
+}));
 function useTable(records, headCells) {
-  const TblContainer = (props) => <Table>{props.children}</Table>;
+  const classes = useStyles();
+  const TblContainer = (props) => (
+    <Table className={classes.table}>{props.children}</Table>
+  );
   const TblHead = (props) => {
     return (
       <TableHead>

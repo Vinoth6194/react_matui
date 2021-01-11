f1219f8ca6ec9979fa6de3910f15fbe4c85ac9c5
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-11 17:23:55 +0530

    dialog styled using themes

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,10 +1,26 @@
-import { Dialog, DialogContent, DialogTitle } from "@material-ui/core";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  makeStyles,
+} from "@material-ui/core";
 import React from "react";
-
+const useStyles = makeStyles((theme) => ({
+  dialogWrapper: {
+    position: "absolute",
+    top: theme.spacing(5),
+    padding: theme.spacing(2),
+  },
+}));
 function Popup(props) {
   const { title, children, openPopup, setOpenPopup } = props;
+  const classes = useStyles();
   return (
-    <Dialog open={openPopup}>
+    <Dialog
+      open={openPopup}
+      maxWidth="md"
+      classes={{ paper: classes.dialogWrapper }} //* internally the dialog component uses paper component, to target paper we have used classes instead of className as earlier..
+    >
       <DialogTitle>Dialog title</DialogTitle>
       <DialogContent>{children}</DialogContent>
     </Dialog>

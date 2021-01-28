876ea7536a6cc3ab04e2e884dcd049dfd7ea59dc
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-29 00:51:45 +0530

    snackbar alert styled with jss

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Alert } from "@material-ui/lab";
-
+import { makeStyles } from "@material-ui/core";
 import Snackbar from "@material-ui/core/Snackbar";
-
+const useStyles = makeStyles((theme) => ({
+  root: {
+    top: theme.spacing(9),
+  },
+}));
 function Notification(props) {
   const { notify, setNotify } = props;
+  const classes = useStyles();
   return (
     <>
       <Snackbar
+        className={classes.root}
         open={notify.isOpen}
         autoHideDuration={3}
         anchorOrigin={{ vertical: "top", horizontal: "right" }}

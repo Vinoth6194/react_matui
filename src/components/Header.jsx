85c83ab6f85a7486484c63b484cfc2548f2b1224
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-06-14 16:57:04 +0530

    changed the appbar bkrnd with custom css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,26 @@ import {
   Grid,
   IconButton,
   InputBase,
+  makeStyles,
   Toolbar,
 } from '@material-ui/core';
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+
+const useStyles = makeStyles({
+  //*The root here is an userdef classname
+  root: {
+    backgroundColor: 'white',
+  },
+});
+
 function Header() {
+  const classes = useStyles();
   return (
-    <AppBar position="static">
+    <AppBar position="static" className={classes.root}>
+      //*root class gets applied to the Matui comp as it is the recent rendered
+      //*style
       <Toolbar>
         <Grid container>
           <Grid item>

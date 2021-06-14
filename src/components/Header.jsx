bc05364f581a93de155629a462566e184b345d8b
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-06-14 16:34:56 +0530

    badges added in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,39 @@
-import { AppBar, Grid, Toolbar } from '@material-ui/core';
-
+import {
+  AppBar,
+  Badge,
+  Grid,
+  IconButton,
+  InputBase,
+  Toolbar,
+} from '@material-ui/core';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
 function Header() {
   return (
     <AppBar position="static">
       <Toolbar>
         <Grid container>
-          <Grid item sm={6} style={{ border: '1px solid red' }}></Grid>
-          <Grid item sm={6} style={{ border: '1px solid green' }}></Grid>
+          <Grid item sm={6} style={{ border: '2px solid red' }}>
+            <InputBase />
+          </Grid>
+          <Grid item sm={6} style={{ border: '2px solid green' }}>
+            <IconButton>
+              <Badge badgeContent={4} color="secondary">
+                <NotificationsNoneIcon />
+              </Badge>
+            </IconButton>
+            <IconButton>
+              <Badge badgeContent={4} color="primary">
+                <ChatBubbleOutlineIcon />
+              </Badge>
+            </IconButton>
+            <IconButton>
+              <Badge>
+                <PowerSettingsNewIcon />
+              </Badge>
+            </IconButton>
+          </Grid>
         </Grid>
       </Toolbar>
     </AppBar>
